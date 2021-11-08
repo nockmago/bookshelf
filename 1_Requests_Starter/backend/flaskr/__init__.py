6cf92b486a3c73364b60c03ddb120a1c168ad88f
Nicola Maglio <nicolamaglio8@gmail.com>
2021-11-08 11:21:25 +0100

    First commit

--- 1_Requests_Starter/backend/flaskr/__init__.py
+++ 1_Requests_Starter/backend/flaskr/__init__.py
@@ -39,11 +39,60 @@ def create_app(test_config=None):
     #         Response body keys: 'success', 'books' and 'total_books'
     # TEST: When completed, the webpage will display books including title, author, and rating shown as stars
 
+    def paginate_books(request, selection): 
+        page = request.args.get('page', 1, type=int)
+        start = (page - 1) * BOOKS_PER_SHELF
+        end = start + BOOKS_PER_SHELF
+        formatted_books = [book.format() for book in selection]
+        current_books = formatted_books[start:end]
+
+        return current_books
+
+
+    @app.route('/books')
+    def get_books():
+        selection = Book.query.all()
+        current_books = paginate_books(request, selection)
+
+        if len(current_books) == 0: 
+            abort(404)
+
+        return jsonify({
+            'success': True, 
+            'books': current_books,
+            'total_books': len(Book.query.all())
+        })
+
+
     # @TODO: Write a route that will update a single book's rating.
     #         It should only be able to update the rating, not the entire representation
     #         and should follow API design principles regarding method and route.
     #         Response body keys: 'success'
     # TEST: When completed, you will be able to click on stars to update a book's rating and it will persist after refresh
+
+    app.route('/books/<int:book_id>', methods=['PATCH'])
+    def update_book(book_id): 
+        
+        body = request.get_json()
+       
+        try: 
+            book = Book.query.filter(Book.id == book_id).one_or_none()
+            
+            if book is None: 
+                abort(404)
+            
+            if 'rating' in response: 
+                book.rating = int(body.get('rating'))
+            
+            book.update()
+
+            return jsonify({
+            'success': True,
+            'id': book.id
+            })
+
+        except:
+            abort(400)
 
     # @TODO: Write a route that will delete a single book.
     #        Response body keys: 'success', 'deleted'(id of deleted book), 'books' and 'total_books'
